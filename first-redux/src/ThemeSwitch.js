f17768b250051e41ef260d60db36255d685de10d
hbw-cloud <2316570512@qq.com>
2019-02-07 00:06:49 +0800

    :notes:

---
+++ first-redux/src/ThemeSwitch.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+
+export default class ThemeSwitch extends Component {
+  render() {
+    return (
+      <div>
+        <button>Red</button>
+        <button>Blue</button>
+      </div>
+    )
+  }
+}
--- first-redux/src/index.js
+++ first-redux/src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import ReactDOM from 'react-dom'
+import Header from './Header'
+import Content from './Content'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Index extends Component {
+    render () {
+        return (
+            <div>
+                <Header />
+                <Content />
+            </div>
+        )
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Index />, document.getElementById('root'));
+
