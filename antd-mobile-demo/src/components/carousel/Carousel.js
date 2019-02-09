4b21d4afe46e4eb1771c499eb1ea419ae82e24a6
hbw-cloud <2316570512@qq.com>
2019-02-09 20:03:53 +0800

    antd-mobile  虽然比较多的自定义属性 但是体验较差

--- antd-mobile-demo/src/components/carousel/Carousel.js
+++ antd-mobile-demo/src/components/carousel/Carousel.js
@@ -16,17 +16,21 @@ export default class Swiper extends Component {
   }
   render() {
     return (
+      <WingBlank>
         <Carousel
+        style={{width: '375px', height: '80px'}}
           autoplay={false}
-          infinite
-          slideWidth="600px"
-          beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
-          afterChange={index => console.log('slide to', index)}
+          infinite={true}
+          dotActiveStyle={{color: 'green', width: '16px', "border-radius": '3px',border: 0}}
+          dotStyle={{color: 'red',backgroundColor:'rgba(0,0,0,0)',width: '6px', height: '6px',border: 'solid 1px red'}}
+          // beforeChange={() => {console.log('1111')}}
+          // afterChange={index => console.log('slide to', index)}
         >
           {this.state.data.map(val => (
-            <div style={{height: 100+'px',width: 300+'px',backgroundColor: 'blue'}} key={val}><h1>{val}</h1></div>
+            <div style={{height: 100+'px',width: 375+'px',backgroundColor: 'blue'}} key={val}><h1>{val}</h1></div>
           ))}
         </Carousel>
+        </WingBlank>
     );
   }
 }
